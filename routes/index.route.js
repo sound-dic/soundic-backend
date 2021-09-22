38379e8f44ad59e88e0f2785ca407f88bd781bd3
Tensiya <40755203+T2SU@users.noreply.github.com>
2021-09-22 16:11:13 +0900

    modify path

--- routes/index.route.js
+++ routes/index.route.js
@@ -4,7 +4,7 @@ const user = require('./user/index.route');
 
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.send('<a href=/auth/kakao>카카오로그인</a><br/><a href=/auth/google>구글로그인</a>');
+  res.send('<a href=/user/auth/kakao>카카오로그인</a><br/><a href=/user/auth/google>구글로그인</a>');
 });
 
 router.use('/user', user);
